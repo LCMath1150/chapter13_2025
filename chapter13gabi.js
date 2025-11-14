9060df17cbe29f60a4be7f510f4decfc13418e9b
LCMath1150 <123030207+LCMath1150@users.noreply.github.com>
2025-11-14 08:26:33 -0600

    Add files via upload

---
+++ chapter13gabi.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const jsonPath = path.join(__dirname,  'paintings.json');
+let paintings;
+fs.readFile(jsonPath, (err,data)=> {
+    if (err)
+    console.log('Unable to read json data file');
+else 
+paintings = JSON.parse(data);
+});
+
+app.get('/', (req,resp) => { resp.json(paintings)});
+
+
+
+app.get('/artist/:artistID',(req,resp) => {
+    const searchartistID = req.params.artistID();
+    const matches = paintings.filter(obj => searchartistID == obj.paintings.artist.artistID);
+    resp.json(matches);
+});
+
+
+
+
+
+let port = 8080;
+app.listen(port, () => {
+    console.log("Server running at port = " + port);
+});
